42b917df8c09dac6f26f1f478871b9581ad95064
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2024-11-29 19:39:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age)
+	{
+		this._name = name;
+		this._age = age;
+	}
 
-class Student extends Person {}
+	get name(){
+		return this._name;
+	}
 
-class Teacher extends Person {}
+	set age(newAge) {
+        if (typeof newAge === "number" && newAge >= 0) {
+          this._age = newAge;
+        } else {
+          console.error("Please enter a valid age (non-negative number).");
+        }
+    }
+
+    get age(){
+        return this._age;
+    }
+}
+
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`)
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
